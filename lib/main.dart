11ea852719aefa6722ba0998dcdbb26a18f283be
Lardy Marc <marc.lardy@niji.fr>
2021-01-19 11:17:29 +0100

    Remove gmaps

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-final CameraPosition _kGooglePlex = CameraPosition(
-  target: LatLng(37.42796133580664, -122.085749655962),
-  zoom: 14.4746,
-);
 
 void main() => runApp(MyApp());
 
@@ -12,11 +6,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Directionality(
-    textDirection: TextDirection.ltr,
-    child: GoogleMap(
-      key: Key('testmla'),
-      mapType: MapType.normal,
-      initialCameraPosition: _kGooglePlex,
-    ));
+      textDirection: TextDirection.ltr,
+      child: Text('testmla')
+    );
   }
 }
